ea01f12e8375cab1cf91794dc18eb0cf93c4f317
elyzaco <mikhaelaco@gmail.com>
2025-03-26 01:25:45 +0000

    first commit

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -16,9 +16,8 @@ interface ProfileData {
  */
 const profileData: ProfileData = {
   typewriterName: [
-    "Heyo peeps, I'm Will.",
-    "I love to learn.",
-    "William De Rocco :)",
+    "Hi everyone, I'm Ella!",
+    "Ella Co :)"
   ],
   graduatingClass: 2025,
   majorMinor: "B.A. in Computer Science, Minor in Economics",
